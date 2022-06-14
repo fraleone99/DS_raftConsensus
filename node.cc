d4fbfa3cd7e2e5531c1a41275b1a27cd017525f4
fraleone99 <francesco8.leone@mail.polimi.it>
2022-06-14 11:54:29 +0200

    leader el

--- node.cc
+++ node.cc
@@ -8,6 +8,7 @@
 #include <omnetpp.h>
 #include <iostream>
 #include <cmath>
+#define FSM_DEBUG    // enables debug output from FSMs
 
 #include "raftMessage_m.h"
 #include "RequestVote_m.h"
@@ -24,16 +25,21 @@ class node : public cSimpleModule
   protected:
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
-    virtual RequestVote *generateMessage();
+    virtual RequestVote *generateRequestVote();
     virtual RequestVote *grantVote(RequestVote* msg);
     virtual RequestVote *refuseVote(RequestVote* msg);
+    virtual AppendEntry *refuseAppend(AppendEntry* msg);
+    virtual AppendEntry *acceptAppend(AppendEntry* msg);
+    virtual AppendEntry *generateAppendEntry();
 
     float timer;
+    cMessage *timerMsg = new cMessage("TimerExpired");
     int nodesNumber;
+    int id;
 
      //Persistent state
      int currentTerm = 0; //increases monotonically
-     int votedFor;
+     int votedFor = -1;
      int log[50];
      int voteReceived = 0;
 
@@ -48,9 +54,10 @@ class node : public cSimpleModule
     cFSM fsm;
     enum{
 
-        FOLLOWER = 0,
-        CANDIDATE = FSM_Steady(1),
-        LEADER = FSM_Steady(2),
+        INIT = 0,
+        FOLLOWER = FSM_Steady(1),
+        CANDIDATE = FSM_Steady(2),
+        LEADER = FSM_Steady(3),
     };
 
 
@@ -61,14 +68,14 @@ Define_Module(node);
 void node::initialize()
 {
     nodesNumber = gateSize("gate")+1;
+    id = getIndex();
 
     fsm.setName("fsm");
-    cMessage *msg = new cMessage("start");
     timer = uniform(0.150,0.300);
 
     nextIndex = new int[nodesNumber];
     matchIndex = new int[nodesNumber];
-    scheduleAt(0.0 + timer, msg);
+    scheduleAt(0.0, timerMsg);
 
 
 }
@@ -78,64 +85,100 @@ void node::handleMessage(cMessage *msg)
 
     FSM_Switch(fsm)
     {
-
+        case FSM_Exit(INIT):
+                 rescheduleAt(simTime()+timer, timerMsg);
+                 FSM_Goto(fsm,FOLLOWER);
+                 break;
         case FSM_Exit(FOLLOWER):
                 if(msg -> isSelfMessage()){
                     currentTerm++;
                     FSM_Goto(fsm, CANDIDATE);
                 }
-                else
-                {
-                    FSM_Goto(fsm, FOLLOWER);
-                }
+                else{
+                    if(RequestVote *v = dynamic_cast<RequestVote*>(msg)){
+                        if(v->getArgs().term >= currentTerm && votedFor==-1){
+                            currentTerm = v->getArgs().term;
+                            votedFor = v->getArgs().candidateId;
+                            send(grantVote(v), "gate$o", msg->getArrivalGate()->getIndex());
 
+                        }else{
+                            send(refuseVote(v), "gate$o", msg->getArrivalGate()->getIndex());
+                        }
+                        FSM_Goto(fsm, FOLLOWER);
+
+                    }else if(AppendEntry *v = dynamic_cast<AppendEntry*>(msg)){
+                        if(v->getArgs().term >= currentTerm)
+                            send(acceptAppend(v), "gate$o", msg->getArrivalGate()->getIndex());
+                        else
+                            send(refuseAppend(v), "gate$o", msg->getArrivalGate()->getIndex());
+
+                    }
+                    FSM_Goto(fsm, FOLLOWER);
+                    cancelEvent(timerMsg);
+                }
                 break;
         case FSM_Enter(FOLLOWER):
                 bubble("FOLLOWER");
-                /*if(msg->isSelfMessage()) {
-                    int n = gateSize("gate");
-                    for(int k=0; k<n; k++) {
-                        raftMessage *msg = generateMessage();
-                        send(msg, "gate$o", k);
-                    currentTerm++;
-                    FSM_Goto(fsm,CANDIDATE)
-                    }
-                } else {
-                    raftMessage *rmsg = check_and_cast<raftMessage *>(msg);
-                    int source = rmsg->getArrivalGate()->getIndex();
-                    send(rmsg, "gate$o", source);
-                }*/
+                rescheduleAt(simTime()+timer, timerMsg); //resetTimer
                 break;
-        case FSM_Exit(CANDIDATE):
-                if(voteReceived > ceil(nodesNumber/2))
-                    FSM_Goto(fsm, LEADER);
-                if(RequestVote *v = dynamic_cast<RequestVote*>(msg)){
-                    if(v->getArgs().term >= currentTerm){
-                        votedFor = v->getArgs().candidateId;
-                        send(grantVote(v), "gate$o", msg->getArrivalGate()->getIndex());
-                        FSM_Goto(fsm, FOLLOWER);
 
-                    }
+
+        case FSM_Exit(CANDIDATE):
+                if(msg -> isSelfMessage()){
+                    currentTerm++;
+                    votedFor = -1;
+                    (EV << "reset " << id);
                 }
-                     bubble("another one");
-                FSM_Goto(fsm, CANDIDATE);
-                break;
+                if(RequestVote *v = dynamic_cast<RequestVote*>(msg)){
+                    (EV << "Vote received " << voteReceived );
+                          if(v->getArgs().candidateId == id){
+                              if(v->getRes().voteGranted){
+
+                                  voteReceived++;
+                              }
+                          }
+                          else{
+                              send(refuseVote(v), "gate$o", msg->getArrivalGate()->getIndex());
+                          }
+                 }
+                 if(voteReceived >= ceil(nodesNumber/2))
+                     FSM_Goto(fsm, LEADER);
+                 else
+                     FSM_Goto(fsm, CANDIDATE);
+                 break;
         case FSM_Enter(CANDIDATE):
                 bubble("CANDIDATE");
-                votedFor = getIndex();
-                int n = gateSize("gate");
-                for(int k=0; k<n; k++) {
-                    RequestVote *msg = generateMessage();
-                    send(msg, "gate$o", k);
+                if(votedFor == -1){
+                    for(int k=0; k<nodesNumber-1; k++) {
+                        RequestVote *msg = generateRequestVote();
+                        send(msg, "gate$o", k);
+                    }
+                    votedFor =2;
+                    voteReceived++;
+                    rescheduleAt(simTime()+timer, timerMsg); //resetTimer
                 }
                 break;
+        case FSM_Exit(LEADER):
+                FSM_Goto(fsm, LEADER);
+                break;
+        case FSM_Enter(LEADER):
+                bubble("LEADER");
+            for(int k=0; k<nodesNumber-1; k++) {
+                AppendEntry *msg = generateAppendEntry();
+                send(msg, "gate$o", k);
+            }
+                break;
+
+
+
 
 
 
 
     }
 }
-RequestVote *node::generateMessage()
+
+RequestVote *node::generateRequestVote()
 {
     RequestVote* msg = new RequestVote();
     RequestVote_Args args;
@@ -173,6 +216,48 @@ RequestVote *node::refuseVote(RequestVote* msg)
 
     return msg;
 }
+
+AppendEntry *node::generateAppendEntry()
+{
+    AppendEntry* msg = new AppendEntry();
+    AppendEntry_Args args;
+
+    args.term = currentTerm;
+    args.leaderId = id;
+
+
+    msg->setArgs(args);
+
+    return msg;
+}
+
+AppendEntry *node::acceptAppend(AppendEntry* msg)
+{
+
+    AppendEntry_Results res;
+
+    res.success= true;
+    res.term = currentTerm;
+
+    msg->setRes(res);
+
+    return msg;
+}
+
+AppendEntry *node::refuseAppend(AppendEntry* msg)
+{
+
+    AppendEntry_Results res;
+
+    res.success= false;
+    res.term = currentTerm;
+
+    msg->setRes(res);
+
+    return msg;
+}
+
+
 
 
 
